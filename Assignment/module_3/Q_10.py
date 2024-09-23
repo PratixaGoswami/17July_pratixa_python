4a3e7d7b20c20280bef2d487a8cff9fe13094988
PratixaGoswami <167281022+PratixaGoswami@users.noreply.github.com>
2024-09-23 22:02:21 +0530

    Update Q_10.py

--- Assignment/module_3/Q_10.py
+++ Assignment/module_3/Q_10.py
@@ -1,21 +1,7 @@
 # Write a Python program to count the frequency of words in a file. 
 
 from collections import Counter
-
-<<<<<<< HEAD
-with open("studentdata.txt", "r") as f:
-    words = f.read().lower().split()
-word_count = Counter(words)
-for word, count in word_count.items():
-    print(f"{word}: {count}")
-=======
-# with open("studentdata.txt", "r") as f:
-#     words = f.read().lower().split()
-# word_count = Counter(words)
-# for word, count in word_count.items():
-#     print(f"{word}: {count}")
->>>>>>> e87b1a4 (completed)
-    
+   
 f=open("studentdata.txt","r")
 words=f.read().split()
 word_count=Counter(words)
